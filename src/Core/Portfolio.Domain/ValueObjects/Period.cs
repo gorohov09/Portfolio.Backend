fd58aeb0f0d55813ca43e67b3574ea3a3b0c0d32
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2024-03-30 16:11:03 +0300

    #48 - Класс, представляющий период

---
+++ src/Core/Portfolio.Domain/ValueObjects/Period.cs
@@ -0,0 +1,60 @@
+using Portfolio.Domain.Exceptions;
+
+namespace Portfolio.Domain.ValueObjects
+{
+	/// <summary>
+	/// Период
+	/// </summary>
+	public class Period : ValueObject
+	{
+		private DateTime _startDate;
+		private DateTime _endDate;
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		/// <param name="startDate">Дата начала</param>
+		/// <param name="endDate">Дата окончания</param>
+		public Period(
+			DateTime startDate,
+			DateTime endDate = default)
+		{
+			StartDate = startDate;
+			EndDate = endDate == default ? startDate : endDate;
+		}
+
+		/// <summary>
+		/// Дата начала
+		/// </summary>
+		public DateTime StartDate
+		{
+			get => _startDate;
+			set => _startDate = value == default
+				? throw new RequiredFieldNotSpecifiedException("Дата начала")
+				: value;
+		}
+
+		/// <summary>
+		/// Дата окончания
+		/// </summary>
+		public DateTime EndDate
+		{
+			get => _endDate;
+			set => _endDate = value == default
+				? throw new RequiredFieldNotSpecifiedException("Дата окончания")
+				: value;
+		}
+
+		/// <summary>
+		/// Является ли дата начала и дата окончания одним днем
+		/// </summary>
+		public bool IsOneDay => StartDate == EndDate;
+
+		/// <inheritdoc/>
+		protected override IEnumerable<object> GetEqualityComponents()
+		{
+			yield return StartDate;
+			yield return EndDate;
+		}
+	}
+}
